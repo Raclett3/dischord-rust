c2ac95e150d06161a59199ccb012990cff297c99
Raclett3 <im@s283.net>
2020-03-26 18:59:46 +0900

    Modify the behavior of the bot
    The bot now sends back the execution result of the composer.

--- src/bot.ts
+++ src/bot.ts
@@ -1,4 +1,7 @@
 import Discord from 'discord.js';
+import { resolve as resolvePath } from 'path';
+
+import { spawn } from './async-process';
 
 type MessageListener = (client: Discord.Client, message: Discord.Message) => any;
 
@@ -9,13 +12,22 @@ export async function login(token: string, listener: MessageListener) {
 }
 
 export async function ready(token: string) {
-  await login(token, (client, message) => {
+  await login(token, async (client, message) => {
     if (!client.user || client.user.id === message.author.id || message.author.bot) {
       return;
     }
 
-    if (message.content.toLowerCase().includes('ping')) {
-      message.channel.send('Pong!');
+    const prefix = 'dc!';
+
+    if (message.content.slice(0, prefix.length) !== prefix) {
+      return;
     }
+
+    const command = message.content.slice(prefix.length);
+    const child = spawn(resolvePath(process.cwd(), 'composer/target/debug/dischord_rust'));
+    await child.writeStdin(command);
+    const result = await child.end();
+
+    await message.channel.send('', { files: [{ name: 'result.txt', attachment: result }] });
   });
 }
